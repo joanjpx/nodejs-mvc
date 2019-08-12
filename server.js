79d85e97beb028660c4f531f33450f0d7ab20ffd
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-12 17:07:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,11 @@ var express         = require("express"),
     router          = express.Router(),
     mongoose        = require('mongoose');
 
-    //const jwt = require('_helpers/jwt');
-    const errorHandler = require('_helpers/error-handler');
+//const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
 
 // Setup Connection to DB
-  var mongoDB = '127.0.0.1/tvshows';
-  mongoose.connect('mongodb://'+process.env.DB_HOST+process.env.DB_DATABASE,{ useNewUrlParser: true });
+mongoose.connect('mongodb://'+process.env.DB_HOST+process.env.DB_DATABASE,{ useNewUrlParser: true });
 
 // Middlewares
   app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,21 +26,18 @@ var express         = require("express"),
   app.use(router);
   app.use(cors());
 
-  //Controllers
-  //var TvShowsController = require('controller/tvshows');
-
 // API routes
   router.get('/',function(req, res)
   {
     res.status(200).jsonp("Hello world!");
   });
   
-  //APP ROUTES
-  app.use('/tvshows',require('routes/TvShowsRoutes'));
+//APP ROUTES
+    app.use('/tvshows',require('routes/TvShowsRoutes'));
 
 // global error handler
     app.use(errorHandler);
-    
+
 // Start server
     app.listen(process.env.PORT, function(){
         console.log("Node server running on http://localhost:"+process.env.PORT);
